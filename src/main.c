49d55151a945327cd00ffeaf1d3237c35744b830
moussacodes <0x07d2@proton.me>
2023-12-12 10:21:51 +0100

    stocker les commands dans des struct

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <time.h>
 #include "common.h"
@@ -9,23 +10,25 @@
 
 typedef struct
 {
-    char *content;
+    char **content;
     struct tm time;
     int fail;
     u_int64_t pid;
-} cmd;
+} Cmd;
 
-cmd *parse_command(char *cmd, size_t input_size)
+Cmd *parse_command(char *cmd, size_t input_size, int *nb_token)
 {
-
-    //TODO: should implement a tokenizer to parse the input
+    // TODO: should implement a tokenizer to parse the input
     /*
-    * first check if the command is a local one (exit, cd, ect...)
-    * or else load it from /usr/bin or for PATH
-    */
+     * first check if the command is a local one (exit, cd, ect...)
+     * or else load it from /usr/bin or for PATH
+     */
+    char **tokens = NULL;
     char buffer[MAX_BUFFER];
-    int bufferIndex = 0;
+    int buffer_index = 0;
     int index = 0;
+    *nb_token = 0; // Initialize nb_token to zero
+
     while (cmd[index] != '\0')
     {
         char currentChar = cmd[index];
@@ -35,26 +38,57 @@ cmd *parse_command(char *cmd, size_t input_size)
             index++;
             continue;
         }
-
         if (IS_PIPE_OPRATOR(currentChar))
         {
-            buffer[bufferIndex] = currentChar;
-            buffer[bufferIndex + 1] = '\0'; // Null-terminate the buffer
-            bufferIndex = 0;
+            buffer[buffer_index] = currentChar;
+            buffer[buffer_index + 1] = '\0'; // Null-terminate the buffer
+            tokens = realloc(tokens, (*nb_token + 1) * sizeof(char *));
+            tokens[*nb_token] = strdup(buffer);
+            buffer_index = 0;
+            buffer[buffer_index] = '\0';
+            (*nb_token)++;
             index++;
             continue;
         }
-        buffer[bufferIndex++] = currentChar;
-        buffer[bufferIndex] = '\0';
-        bufferIndex = 0;
+        if (isalpha(currentChar))
+        {
+            while (isalnum(cmd[index]) || cmd[index] == '_' || isalpha(cmd[index]))
+            {
+                buffer[buffer_index++] = cmd[index++];
+            }
+            buffer[buffer_index] = '\0';
+
+            tokens = realloc(tokens, (*nb_token + 1) * sizeof(char *));
+            tokens[*nb_token] = strdup(buffer);
+            buffer_index = 0;
+            buffer[buffer_index] = '\0';
+            (*nb_token)++;
+            continue;
+        }
+
         index++;
     }
+
+    Cmd *command = malloc(sizeof(Cmd));
+    command->content = malloc((*nb_token) * sizeof(char *));
+    for (int i = 0; i < (*nb_token); i++)
+    {
+        command->content[i] = strdup(tokens[i]);
+    }
+
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    command->time = tm;
+
+    free(tokens); // Ne pas oublier de libérer la mémoire allouée pour tokens
+    return command;
 }
 
 int main()
 {
     char *input = NULL;
     size_t input_size = 0;
+    int nb_token = 0;
 
     while (1)
     {
@@ -78,8 +112,15 @@ int main()
         {
             continue;
         }
-
-        fprintf(stdout, "You entered: %s\n", input);
+        Cmd *cmd = parse_command(input, input_size, &nb_token);
+        for (int i = 0; i < nb_token; i++)
+        {
+            printf("%s", cmd->content[i]);
+        }
+        for (int i = 0; i < nb_token; i++)
+        {
+            free((void *)cmd->content[i]);
+        }
     }
 
     free(input);
