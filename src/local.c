d0021668b81f156e263d46030b1194698ceeb3d3
moussacodes <0x07d2@proton.me>
2023-12-13 01:09:48 +0100

    docs: added coments

--- src/local.c
+++ src/local.c
@@ -24,74 +24,25 @@ void remove_last(char *path)
 
 
 char* fixPath(const char* path) {
-    // Copier la chaîne d'entrée pour la modification
-    char modifiedPath[strlen(path) + 1];
+     char modifiedPath[strlen(path) + 1];
     strcpy(modifiedPath, path);
 
-    // Trouver et supprimer les occurrences de "../"
-    char* prevOccurrence = NULL;
+     char* prevOccurrence = NULL;
     char* occurrence = strstr(modifiedPath, "../");
     while (occurrence != NULL) {
-        // S'assurer que l'occurrence se trouve au début de la chaîne
-        if (occurrence == modifiedPath || *(occurrence - 1) == '/') {
-            // Supprimer l'occurrence de "../"
-            memmove(occurrence, occurrence + 3, strlen(occurrence + 3) + 1);
+         if (occurrence == modifiedPath || *(occurrence - 1) == '/') {
+             memmove(occurrence, occurrence + 3, strlen(occurrence + 3) + 1);
 
-            // Rechercher la prochaine occurrence
-            prevOccurrence = occurrence;
+             prevOccurrence = occurrence;
             occurrence = strstr(modifiedPath, "../");
         } else {
-            // Si l'occurrence n'est pas au début, passer à la suite
-            occurrence = strstr(occurrence + 1, "../");
+             occurrence = strstr(occurrence + 1, "../");
         }
     }
 
-    // Vous pouvez maintenant utiliser modifiedPath comme chemin corrigé
-    return strdup(modifiedPath);
+     return strdup(modifiedPath);
 }
-
-// static char *concat_strings(char **array, int size)
-// {
-//     int total_length = 0;
-//     for (int i = 0; i < size; i++)
-//     {
-//         total_length += strlen(array[i]);
-//     }
-
-//     char *result = (char *)malloc(total_length + 1);
-//     if (result == NULL)
-//     {
-//         perror("Memory allocation failed");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     result[0] = '\0';
-//     for (int i = 0; i < size; i++)
-//     {
-//         if (strcmp(array[i], "..") != 0)
-//         {
-//             strcat(result, array[i]);
-//         }
-//     }
-//     return result;
-// }
-
-// char *remove_prefix(char *path)
-// {
-//     char *token;
-//     char **p_list = NULL;
-//     int n = 0;
-//     token = strtok(path, "/");
-//     while (token != NULL)
-//     {
-//         p_list = realloc(p_list, (n + 1) * sizeof(char *));
-//         p_list[n] = strdup(token);
-//         token = strtok(NULL, "/");
-//     }
-
-//     concat_strings(p_list, n + 1);
-// }
-
+ 
 void cd(char **content, dir_info *cur_dir, int nb_tokens)
 {
     char prefix[MAXPATH];
@@ -111,8 +62,8 @@ void cd(char **content, dir_info *cur_dir, int nb_tokens)
             exit(EXIT_FAILURE);
         }
 
-        printf("\npath: %s\n", path);
-        int index = 0;
+        //TODO: check for cases (., ./, /..)
+         int index = 0;
         while (path[index] != '\0')
         {
             if (path[index] == '.')
@@ -160,12 +111,10 @@ void cd(char **content, dir_info *cur_dir, int nb_tokens)
             }
             else
             {
-                printf("%s", path);
-                snprintf(prefix, sizeof(prefix), "%s/%s", cur_dir->cur_dir, path);
+                 snprintf(prefix, sizeof(prefix), "%s/%s", cur_dir->cur_dir, path);
                 if (strstr(path, "..") == NULL)
                 {
-                    printf("new no: :  %s", path);
-                    if (chdir(prefix) != 0)
+                     if (chdir(prefix) != 0)
                     {
                         perror("Error: chdir failed");
                         exit(EXIT_FAILURE);
@@ -174,8 +123,7 @@ void cd(char **content, dir_info *cur_dir, int nb_tokens)
                 else
                 {
                     char *new_p = fixPath(prefix);
-                    printf("new with ::  %s", new_p);
-                    if (chdir(new_p) != 0)
+                     if (chdir(new_p) != 0)
                     {
                         perror("Error: chdir failed");
                         free(new_p);
